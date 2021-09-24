437869796615b6664e0ead37b15147b6698d99cd
Teri-IM <63192118+Teri-IM@users.noreply.github.com>
2021-09-24 20:53:04 +0300

    less2.1.4

--- src/module1/less2_1_4.java
+++ src/module1/less2_1_4.java
@@ -1,7 +1,17 @@
 package module1;
 
+import java.util.Scanner;
 public class less2_1_4 {
     public static void main(String[] Args){
-
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
+        int r = a*c, n=0;
+        b=b*c;
+        while(b>99){b=b-100; r++;}
+        System.out.println(r+" "+b);
     }
 }
+
+//        System.out.println(n);
