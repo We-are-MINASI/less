f6006928f94b2ee0db6da6aad16a627124e2ea72
Teri-IM <63192118+Teri-IM@users.noreply.github.com>
2021-09-25 16:45:00 +0300

    lesss

--- src/module1/less2_1_5.java
+++ src/module1/less2_1_5.java
@@ -6,6 +6,8 @@ public class less2_1_5 {
         Scanner in = new Scanner(System.in);
         int a = in.nextInt();
         int h=a/3600,m=(a-h*3600)/60,s=a-h*3600-m*60;
-        System.out.println(h+":"+m+":"+s);
+        while(h>=24){h=h-24;}
+        if(m>9&&s<10){System.out.println(h+":"+m+":0"+s);} else{if(m<10&&s>9){System.out.println(h+":0"+m+":"+s);} else{if(m<10&&s<10){System.out.println(h+":0"+m+":0"+s);}else{System.out.println(h+":"+m+":"+s);}}}
     }
 }
+//осторожно говнокод line 9
