f07b680a8dde015a296bfe258b86d16e28e914f8
Teri-IM <63192118+Teri-IM@users.noreply.github.com>
2021-09-25 21:25:45 +0300

    lesss

--- src/module1/less4_1_2.java
+++ src/module1/less4_1_2.java
@@ -1,9 +1,10 @@
 package module1;
 
 import java.util.Scanner;
-
 public class less4_1_2 {
     public static void main(String[] Args){
         Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        if (a>0){int b=(1<<a); System.out.println(b);} else System.out.println(a);
     }
 }
