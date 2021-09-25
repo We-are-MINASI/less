23a32d7c31be52bed78cb1f82bf8d67549ce9e81
Teri-IM <63192118+Teri-IM@users.noreply.github.com>
2021-09-25 15:29:59 +0300

    less2.1.5

--- src/module1/less2_1_5.java
+++ src/module1/less2_1_5.java
@@ -1,7 +1,11 @@
 package module1;
 
+import java.util.Scanner;
 public class less2_1_5 {
     public static void main(String[] Args){
-
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int h=a/3600,m=(a-h*3600)/60,s=a-h*3600-m*60;
+        System.out.println(h+":"+m+":"+s);
     }
 }
