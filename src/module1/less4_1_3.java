bf7ab3b71c86a85f465cbdeb04f67133cf0ecd89
Teri-IM <63192118+Teri-IM@users.noreply.github.com>
2021-09-26 21:10:42 +0300

    lesss

---
+++ src/module1/less4_1_3.java
@@ -0,0 +1,10 @@
+package module1;
+
+import java.util.Scanner;
+public class less4_1_3 {
+    public static void main(String[] Args){
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+
+    }
+}
---
+++ src/module1/less4_1_5.java
@@ -0,0 +1,11 @@
+package module1;
+
+import java.util.Scanner;
+
+public class less4_1_5 {
+    public static void main(String[] Args){
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+
+    }
+}
