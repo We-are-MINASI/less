5533ff27cd9db0323fdf004140a1b33eee240558
Teri-IM <63192118+Teri-IM@users.noreply.github.com>
2021-09-27 16:56:05 +0300

    lesss

--- src/module1/less2_1_5.java
+++ src/module1/less2_1_5.java
@@ -10,4 +10,4 @@ public class less2_1_5 {
         if(m>9&&s<10){System.out.println(h+":"+m+":0"+s);} else{if(m<10&&s>9){System.out.println(h+":0"+m+":"+s);} else{if(m<10&&s<10){System.out.println(h+":0"+m+":0"+s);}else{System.out.println(h+":"+m+":"+s);}}}
     }
 }
-//осторожно говнокод line 9
+//осторожно говнокод line 10
