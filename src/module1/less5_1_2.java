b19e67a0a578f9507e1c211a04555f5d93bfdae1
Antonovilya2006 <antonov.ilya29102016@gmail.com>
2021-10-11 17:00:41 +0300

    kujhgf

--- src/module1/less5_1_2.java
+++ src/module1/less5_1_2.java
@@ -1,12 +1,12 @@
 package module1;
 import java.util.Locale;
 import java.util.Scanner;
-public class less5_1_1 {
+public class less5_1_2 {
     public static void main(String[] Args) {
         Scanner in = new Scanner(System.in);
         in.useLocale(Locale.US);
         double t = in.nextDouble();
-        if ((t >= 3) && (t <= 8)) {
+        if (((t >= -3) && (t <= 5))||((t>=9)&&(t<=15))) {
             System.out.println("true");
         } else System.out.println("false");
     }
--- src/module1/less5_1_3.java
+++ src/module1/less5_1_3.java
@@ -1,13 +1,13 @@
 package module1;
 import java.util.Locale;
 import java.util.Scanner;
-public class less5_1_1 {
+public class less5_1_3 {
     public static void main(String[] Args) {
         Scanner in = new Scanner(System.in);
         in.useLocale(Locale.US);
         double t = in.nextDouble();
-        if ((t >= 3) && (t <= 8)) {
-            System.out.println("true");
-        } else System.out.println("false");
+        if (((t >= -2) && (t <= 3))||((t>=6)&&(t<=9))) {
+            System.out.println("false");
+        } else System.out.println("true");
     }
 }
