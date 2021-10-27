ebaecb27bdae422c9f9b1b8190449a91f285db60
Teri-IM <63192118+Teri-IM@users.noreply.github.com>
2021-10-27 15:52:19 +0300

    Just a comm

---
+++ src/module1/less9_1_2.java
@@ -0,0 +1,10 @@
+package module1;
+
+import java.util.Scanner;
+
+public class less9_1_2 {
+    public static void main(String[] Args){
+        Scanner in = new Scanner(System.in);
+
+    }
+}
---
+++ src/module1/less9_1_4.java
@@ -0,0 +1,10 @@
+package module1;
+
+import java.util.Scanner;
+
+public class less9_1_4 {
+    public static void main(String[] Args){
+        Scanner in = new Scanner(System.in);
+        
+    }
+}
