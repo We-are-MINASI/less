9afe6f80ba4d8f368a8eb07e5796cca824bb8289
Teri-IM <63192118+Teri-IM@users.noreply.github.com>
2021-10-28 11:19:07 +0300

    lesss

--- src/module1/less4_1_4.java
+++ src/module1/less4_1_4.java
@@ -5,7 +5,15 @@ import java.util.Scanner;
 public class less4_1_4 {
     public static void main(String[] Args){
         Scanner in = new Scanner(System.in);
-        int a = in.nextInt();
-
+        String a = (in.nextLine());
+        String[] nuA = a.split(" ");
+        String f = nuA[0];
+        int f1 = Integer.parseInt(f.trim());
+        String g = nuA[1];
+        int g1 = Integer.parseInt(g.trim()),g2=30-g1;
+        //System.out.println(f1+" "+g1);
+        Integer.toBinaryString(f1);
+        int b=(f1<<g2), c=(b>>g2);
+        System.out.println(c);
     }
 }
--- src/module1/less4_1_5.java
+++ src/module1/less4_1_5.java
@@ -3,9 +3,16 @@ package module1;
 import java.util.Scanner;
 
 public class less4_1_5 {
-    public static void main(String[] Args){
+    public static void main(String[] Args) {
         Scanner in = new Scanner(System.in);
-        int a = in.nextInt();
+        String a = (in.nextLine());
+        String[] nuA = a.split(" ");
+        String f = nuA[0];
+        int f1 = Integer.parseInt(f.trim());
+        String g = nuA[1];
+        int g1 = Integer.parseInt(g.trim()), g2 = g1 + 1;
+        //System.out.println(f1+" "+g1);
+        Integer.toBinaryString(f1);
 
     }
 }
