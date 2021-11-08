0819afa445d51d3f142cc8d1e2b1a37d96490135
Basya-end <91340874+Basya-end@users.noreply.github.com>
2021-11-08 16:02:48 +0300

    less

--- src/module1/less4_1_4.java
+++ src/module1/less4_1_4.java
@@ -1,19 +1,11 @@
 package module1;
 
 import java.util.Scanner;
-
 public class less4_1_4 {
-    public static void main(String[] Args){
+    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String a = (in.nextLine());
-        String[] nuA = a.split(" ");
-        String f = nuA[0];
-        int f1 = Integer.parseInt(f.trim());
-        String g = nuA[1];
-        int g1 = Integer.parseInt(g.trim()),g2=30-g1;
-        //System.out.println(f1+" "+g1);
-        Integer.toBinaryString(f1);
-        int b=(f1<<g2), c=(b>>g2);
-        System.out.println(c);
+        int a = in.nextInt();
+        int i = in.nextInt();
+        System.out.println((a ^ (int)Math.pow(2,i)));
     }
 }
--- src/module1/less4_1_5.java
+++ src/module1/less4_1_5.java
@@ -3,16 +3,10 @@ package module1;
 import java.util.Scanner;
 
 public class less4_1_5 {
-    public static void main(String[] Args) {
+    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String a = (in.nextLine());
-        String[] nuA = a.split(" ");
-        String f = nuA[0];
-        int f1 = Integer.parseInt(f.trim());
-        String g = nuA[1];
-        int g1 = Integer.parseInt(g.trim()), g2 = g1 + 1;
-        //System.out.println(f1+" "+g1);
-        Integer.toBinaryString(f1);
-
+        int a = in.nextInt();
+        int i = in.nextInt();
+        System.out.println(a << (32-i) >>> (32-i));
     }
 }
