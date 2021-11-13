022d89022cb1f1963da2a0be0fd810ba4e811a63
Teri-IM <mot.teri22@gmail.com>
2021-11-13 16:58:14 +0300

    less5_2

--- src/module1/less5_2_5.java
+++ src/module1/less5_2_5.java
@@ -11,6 +11,7 @@ public class less5_2_5 {
         double x1 = Double.parseDouble(f.trim()), d1=x1*x1, x2=x1*(-1);
         String g = nuA[1];
         double y1 = Double.parseDouble(g.trim()), d2=y1*y1, e=d1+d2;
+        if (e<1) {if (y1 > x2 || y1 < x1 || y1 == x1 || y1 == x2){System.out.println("YES");}else{System.out.println("NO");}}else{System.out.println("NO");}
     }
 }
 //System.out.println(f1+"\n"+g1);
