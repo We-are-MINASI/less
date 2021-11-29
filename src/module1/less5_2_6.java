e6c4b2237afbea9bc210e9a2db877f31a7dfc731
Teri-IM <mot.teri22@gmail.com>
2021-11-29 15:34:46 +0300

    wtf

--- src/module1/less5_2_6.java
+++ src/module1/less5_2_6.java
@@ -8,10 +8,10 @@ public class less5_2_6 {
         String a = (in.nextLine());
         String[] nuA = a.split(" ");
         String f = nuA[0];
-        double x1 = Double.parseDouble(f.trim()), x2=x1*x1*2;
+        double x1 = Double.parseDouble(f.trim()), d1=x1*x1, x2=x1*(-1);
         String g = nuA[1];
-        double y1 = Double.parseDouble(g.trim());
-        if (x1<1 || x1==1){if (y1>1-x1 || y1==1-x1){if (x1>0 || x1==0){System.out.println("YES");}else{if (y1<x2 || y1==x2){System.out.println("NO");}else{System.out.println("YES");}}}else{System.out.println("NO");}}else{System.out.println("NO");}
+        double y1 = Double.parseDouble(g.trim()), d2=y1*y1, e=d1+d2;
+        if (e<1) {if (y1 > x2 || y1 < x1 || y1 == x1 || y1 == x2){System.out.println("YES");}else{System.out.println("NO");}}else{System.out.println("NO");}
     }
 }
 //System.out.println(f1+"\n"+g1);
